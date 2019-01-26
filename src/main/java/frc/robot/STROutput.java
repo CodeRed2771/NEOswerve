3722d6aa10fea50df9e38f727baaecd38397d082
Dale Van Voorst <dale@dalevanvoorst.com>
2019-01-26 14:17:48 -0500

    PID controllers

---
+++ src/main/java/frc/robot/STROutput.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+
+/**
+ * This class collects the PID output of the strafe PID
+ * and provides the result to the calling method (usually swerve)
+ */
+public class STROutput implements PIDOutput {
+    double value = 0;
+
+    public void pidWrite(double output) {
+        value = output;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
