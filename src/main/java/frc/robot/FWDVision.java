9001b826293f2f62014e155f0c55cabb45e8b974
Dale Van Voorst <dale@dalevanvoorst.com>
2019-02-04 17:39:29 -0500

    Revised input averaging to also limit the value of new entries to further limit the gyrations.

--- src/main/java/frc/robot/FWDVision.java
+++ src/main/java/frc/robot/FWDVision.java
@@ -14,19 +14,19 @@ import edu.wpi.first.wpilibj.PIDSourceType;
 /**
  * PID Source for vision based distance (FWD) PID
  */
-public class FWDVision implements PIDSource{
+public class FWDVision implements PIDSource {
 
 	double targetArray[];
 	AnalogInput distSensor;
 	int arrayPos = 0;
 	int maxPos = 5;
 
-	public FWDVision(){
+	public FWDVision() {
 		targetArray = new double[maxPos];
 		distSensor = new AnalogInput(1);
 	}
 
-    public PIDSourceType getPIDSourceType() {
+	public PIDSourceType getPIDSourceType() {
 		return PIDSourceType.kDisplacement;
 	}
 
@@ -34,48 +34,70 @@ public class FWDVision implements PIDSource{
 
 	}
 
-	public double pidGet() {
-		double targetSum = 0;
-		int targetCount = 0;
-		double averageDistance = 0;
+	
 
-		targetArray[arrayPos] = Vision.getDistanceFromTarget();
-		
-		// if we just got a zero, blank out the array
-		if (targetArray[arrayPos] == 0){
-			for(int i = 0; i < maxPos; i++){
-				targetArray[i] = 0;
+	public double pidGet() {
+		double maxRateOfChange = 6;
+		double newValue = Vision.getDistanceFromTarget();
+		double currentArrayAverage = arrayAverage();
+
+		if (newValue == 0) {
+			targetArray[arrayPos] = 0;
+		} else {
+			// make sure the new value isn't extremely different than existing values
+			if (currentArrayAverage == 0) { // we don't anything yet, so just take it
+				targetArray[arrayPos] = newValue;
+			} else {
+				if (newValue - currentArrayAverage > maxRateOfChange) {
+					newValue = currentArrayAverage + maxRateOfChange;
+				} else if (currentArrayAverage - newValue > maxRateOfChange) {
+					newValue = currentArrayAverage - maxRateOfChange;
+				}
+
+				targetArray[arrayPos] = newValue;
 			}
 		}
-		arrayPos++;
-		
-		if(arrayPos >= maxPos)
-			arrayPos = 0;
 
-		for (int i = 0; i < maxPos; i++){
-			if (targetArray[i] != 0){
+		// if we just got a zero, blank out the array
+		if (targetArray[arrayPos] == 0) {
+			for (int i = 0; i < maxPos; i++) {
+				targetArray[i] = 0;
+			}
+		} else {
+			arrayPos++;
+
+			if (arrayPos >= maxPos)
+				arrayPos = 0;
+		}
+
+		return arrayAverage();
+
+		// return Vision.getDistanceFromTarget();
+	}
+
+	private double arrayAverage() {
+		double targetSum = 0;
+		int targetCount = 0;
+
+		for (int i = 0; i < maxPos; i++) {
+			if (targetArray[i] != 0) {
 				targetSum += targetArray[i];
 				targetCount++;
 			}
 		}
-		
-		if (targetCount == 0){
-			averageDistance = 0;
+
+		if (targetCount == 0) {
+			return 0;
 		} else {
-			averageDistance = targetSum/targetCount;
+			return targetSum / targetCount;
 		}
-		
-		return averageDistance;
-		
-		// return Vision.getDistanceFromTarget();
 	}
-
-	public double getUSDistance(){
+	public double getUSDistance() {
 		double vPer5MM = 5 / 1024;
-		double rangeInMM = 5*(distSensor.getAverageVoltage()/vPer5MM);
+		double rangeInMM = 5 * (distSensor.getAverageVoltage() / vPer5MM);
 
-		return rangeInMM / 25.4;   // returns inches
-		
+		return rangeInMM / 25.4; // returns inches
+
 	}
 
 }
