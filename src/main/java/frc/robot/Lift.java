88dfab54f9fffa900cdc0d1876836a4b391e51e8
Cymbre <cymbre@mattscott.org>
2019-03-09 15:53:40 -0500

    Stufs

--- src/main/java/frc/robot/Lift.java
+++ src/main/java/frc/robot/Lift.java
@@ -113,18 +113,8 @@ public class Lift {
 		liftMotor.set(ControlMode.MotionMagic, 0);
 	}
 
-	// BTW if this doesn't work Joel and Colton made this so check here first if
-	// anything doesn't work :D
-	// *FUN FACTS FOR TODAY 1. croussants were invented in Austria 2. An Ostrich's
-	// eye is bigger than it's actual brain. 2. A goldfish has a memory span of 3
-	// seconds. 4. I skipped fact number three :D 5. The Hawaiin Islands were
-	// originally called the Sandwich Islands. 6. Bunnies like licorice. 7. Most
-	// dinosaours lived to be more than 100 years old. 8.dalmations are born with
-	// completely white fur, and only grow their spots once they grow older. 9.
-	// 'dreamt' is the only word in the English dictionary that ends in the two
-	// letters 'mt'. 10. This is actually a question for you to think about..."Why
-	// is there a fruit called a grapefruit if there is already a thing called a
-	// grape which is a fruit? P.S THEY DID SURGERY ON A GRAPE"*
+	private static final double HATCH_ACQUIRING = -2000; // WARNING THIS IS WRONG
+	private static final double HATCH_SCORING = -200;
 	private static final double HATCH_LEVEL_1 = -1340;
 	private static final double HATCH_LEVEL_2 = -11000;
 	private static final double HATCH_LEVEL_3 = -22000;
@@ -132,6 +122,14 @@ public class Lift {
 	private static final double CARGO_LEVEL_2 = -14000;
 	private static final double CARGO_LEVEL_3 = -25000;
 
+	public static void getHatchPanel() {
+		liftMotor.set(ControlMode.MotionMagic, HATCH_ACQUIRING);
+	}
+
+	public static void scoreHatchPanel() {
+		liftMotor.set(ControlMode.MotionMagic, liftMotor.getSensorCollection().getQuadraturePosition() - HATCH_SCORING);
+	}
+	
 	public static void goHatchLvl1() {
 		liftMotor.set(ControlMode.MotionMagic, HATCH_LEVEL_1);
 	}
