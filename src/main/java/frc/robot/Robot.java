8ec0e24a51fa304096f500391f61e0c011b8817f
Dale Van Voorst <dale@dalevanvoorst.com>
2019-03-23 08:07:48 -0400

    A couple more tweaks to revert to old drive code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -445,7 +445,7 @@ public class Robot extends TimedRobot {
 	}
 
 	public void disabledInit() {
-		// DriveTrain.allowTurnEncoderReset(); // allows the turn encoders to be
+		DriveTrain.allowTurnEncoderReset(); // allows the turn encoders to be
 											// reset once during disabled
 											// periodic
 		DriveTrain.resetDriveEncoders();
@@ -453,7 +453,7 @@ public class Robot extends TimedRobot {
 	}
 
 	public void disabledPeriodic() {
-		// DriveTrain.resetTurnEncoders(); // happens only once because a flag
+		DriveTrain.resetTurnEncoders(); // happens only once because a flag
 										// prevents multiple calls
 		DriveTrain.disablePID();
 
