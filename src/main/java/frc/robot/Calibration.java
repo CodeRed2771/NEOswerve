a12e68f8aa67b48e24e1836e9efea4919b7adfbb
Dale Van Voorst <dale@dalevanvoorst.com>
2019-03-29 17:26:53 -0400

    wheel adjustment

--- src/main/java/frc/robot/Calibration.java
+++ src/main/java/frc/robot/Calibration.java
@@ -33,7 +33,7 @@ public class Calibration {
 	
 	private final static double DT_A_ABS_ZERO_INITIAL = .699; // COMPEITION
 	private final static double DT_B_ABS_ZERO_INITIAL = .014;
-	private final static double DT_C_ABS_ZERO_INITIAL = .869;
+	private final static double DT_C_ABS_ZERO_INITIAL = .903;
 	private final static double DT_D_ABS_ZERO_INITIAL = .538;
 	
 
